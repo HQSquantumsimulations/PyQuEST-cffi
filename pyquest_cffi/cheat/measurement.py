b46c05ef53aeeaacdd8c595428e71ca4af0db5bd
Nicolas Vogt <Nicolas.Vogt@quantumsimulations.de>
2021-05-06 21:59:40 +0200

    fixing linting error

--- pyquest_cffi/cheat/measurement.py
+++ pyquest_cffi/cheat/measurement.py
@@ -527,7 +527,8 @@ class getRepeatedMeasurement(_PYQUEST):
         number_qubits = qureg.numQubitsRepresented
         probabilities = np.zeros((2**N, ))
         measurement_record = np.zeros((number_measurements, N))
-        return_record = np.zeros((number_measurements, len(qubits_to_readout_index_dict)), dtype=int)
+        return_record = np.zeros(
+            (number_measurements, len(qubits_to_readout_index_dict)), dtype=int)
         if qureg.isDensityMatrix:
             for index in range(2**N):
                 probabilities[index] = np.abs(
